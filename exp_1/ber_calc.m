2b12b7b55b453270ae1a0bed0c86c0106fe4c4e7
Marwan Baghdad <mrwanbaghdad76@gmail.com>
2017-04-21 23:54:45 +0200

    Update ber_calc.m

--- exp_1/ber_calc.m
+++ exp_1/ber_calc.m
@@ -1,31 +1,31 @@
 % init snr values 
 
-for  i = 0:15
-    snr(i+1) = 10^(2*i)/10;
-end
- for i= 1:length(snr)
-    % generate transimited sequence 
-    cur_snr = snr(1);
-    tx_sequence = randi([0,1],1,cur_snr*10e6);
-    % calc ptx 
-    ptx = mean(tx_sequence.^2);
+snr = 0:2:30;
+
+tx_sequence = randi([0,1],1,1e6);
+    % calc ptx
+ptx = mean(tx_sequence.^2);
+
+ for s= 1:length(snr)
+    % generate transimited sequence
+    s
+    cur_snr = 10.^(snr(s)/10);    
     
     % calc noise
-    noise = sqrt(ptx /(2 * cur_snr))*(randn(size(tx_sequence))+i*randn(size(tx_sequence)));
-    
+    noise = sqrt(ptx /(2 * cur_snr))* ( randn(size(tx_sequence)) + (i*randn(size(tx_sequence))));
     % generate rx_sequence
     rx_sequence = tx_sequence + noise;
-    % decision making to one and zero bits
-    for j = 1:length(rx_sequence)
-         if rx_sequence(j) >= 0.5
-             rx_sequence(j) = 1;
-         else
-             rx_sequence(j) = 0;
-         end
-        
-    end
-    [x,y] = biterr(rx_sequence, tx_sequence);
-    ber(i) = y;
+    % decision making to one and zero 
 
+    for k = 1:length(rx_sequence)
+         if (rx_sequence(k)) >= 0.5
+             rx(k) = 1;
+         else
+             rx(k) = 0;
+         end
+    end
+    [x,y] = biterr(tx_sequence,rx);
+    ber(s) = y;
  end
-  semilogy(ber, snr)
+ snr = 0:2:30;
+semilogy(snr, ber);
